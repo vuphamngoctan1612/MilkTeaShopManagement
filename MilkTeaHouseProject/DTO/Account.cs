b45637729ac09f0a27570eaefcd3c74a44e586e4
Anh Tran <trananh22112001@gmail.com>
2020-11-22 11:51:20 +0700

    update folder DTO

---
+++ MilkTeaHouseProject/DTO/Account.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MilkTeaHouseProject.DTO
+{
+    public class Account
+    {
+        private string username;
+        private string password;
+        private bool type;
+
+        public string Username { get => username; set => username = value; }
+        public string Password { get => password; set => password = value; }
+        public bool Type { get => type; set => type = value; }
+
+        public Account(string username, string password, bool type)
+        {
+            this.username = username;
+            this.password = password;
+            this.type = type;
+        }
+
+        public Account(DataRow row)
+        {
+            this.Username = row["USERNAME"].ToString();
+            this.Password = row["PASSWORD"].ToString();
+            this.type = (bool)row["TYPE"];
+        }
+    }
+}
--- MilkTeaHouseProject/DTO/Drink.cs
+++ MilkTeaHouseProject/DTO/Drink.cs
@@ -9,25 +9,33 @@ namespace MilkTeaShopManagement.DTO
 {
     public class Drink
     {
-        int iD;
-        string name;
-        int price;
+        private int iD;
+        private string name;
+        private int categoryID;
+        private int price;
+        private byte[] image;
 
         public string Name { get => name; set => name = value; }
         public int Price { get => price; set => price = value; }
         public int ID { get => iD; set => iD = value; }
+        public int CategoryID { get => categoryID; set => categoryID = value; }
+        public byte[] Image { get => image; set => image = value; }
 
-        public Drink(int id, string name, int price)
+        public Drink(int id, string name, int price, int categoryID)
         {
             this.ID = id;
             this.Name = name;
             this.Price = price;
+            this.categoryID = categoryID;
         }
         public Drink(DataRow row)
         {
             this.ID = (int)row["ID"];
             this.Name = row["Name"].ToString();
             this.Price = (int)row["Price"];
+            this.CategoryID = (int)row["IDCategory"];
+            if (!Convert.IsDBNull(row["Image"]))
+                this.Image = (byte[])row["Image"];
         }
     }
 }
